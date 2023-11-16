8be3d536b869c6f4bd589e909d09f20d1cf559e7
Prakash Prajapati <prakash.sp@accubits.com>
2023-11-16 11:34:34 +0530

    question added

--- nodejsQuestions/async.js
+++ nodejsQuestions/async.js
@@ -1,17 +1,35 @@
-function resolveAfter2Seconds() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve('resolved');
-        }, 2000);
-    });
-}
+// function resolveAfter2Seconds() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve('resolved');
+//         }, 2000);
+//     });
+// }
 
-async function asyncCall() {
-    console.log('calling');
-    const result = await resolveAfter2Seconds();
-    console.log(result);
-    console.log('hello')
-}
+// async function asyncCall() {
+//     console.log('calling');
+//     const result = await resolveAfter2Seconds();
+//     console.log(result);
+//     console.log('hello')
+// }
 
-asyncCall();
+// asyncCall();
+// Event listener for unhandled exceptions
+process.on('uncaughtException', (err) => {
+    console.error(`Uncaught Exception: ${err.message}`);
+    // You may want to perform additional cleanup or logging here
+
+    // Terminate the application (exit with a non-zero code)
+    process.exit(1);
+});
+
+// Simulate an unhandled exception
+setTimeout(() => {
+    throw new Error('Simulated unhandled exception');
+}, 1000);
+
+// This code will continue to execute even if an unhandled exception occurs,
+// but the process.on('uncaughtException') event will catch it.
+console.log('Application is still running...');
+
 
